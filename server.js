313ad9aa8db6e5b636ca15181ea99488822cb5d9
Vicente Torrealba <vicentetorrealba@MacBook-Pro-de-Maria.local>
2026-04-15 12:13:58 -0400

    fix server sin redirect

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`NumberVi web corriendo en puerto ${PORT}`);
